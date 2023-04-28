9f4d5eff0b7583028e559036e83aac8afa67a87c
vinod <vinod.balagoni@celllabs.com>
2023-04-28 18:37:12 +0530

    initial commit

---
+++ src/main/java/com/cicd/cidc/CidcApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.cidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/cidc/CidcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.cidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
